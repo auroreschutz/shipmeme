e7740c2e598c9e65b5775e2923d30627c6042932
Aurore <pro.aurore.schutz@gmail.com>
2023-12-13 20:13:04 +0100

    First Commit

--- test/script.js
+++
@@ -1,9 +0,0 @@
-function validateFileType(){
-    var upFile = document.getElementById("upFile").value;
-    var fileName = document.getElementById("fileName");
-    fileName.innerHTML="Uploaded";
-  
-    prev_img.src = URL.createObjectURL(event.target.files[0]);
-  
-  }
-  
