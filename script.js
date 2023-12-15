c9aadfa64148ecd2ca7cd12e66c631e13a8b0106
Aurore <pro.aurore.schutz@gmail.com>
2023-12-15 20:28:17 +0100

    workable version, still wip

--- script.js
+++
@@ -1,7 +0,0 @@
-function validateFileType(){
-    var upFile = document.getElementsByClassName("upFile").value; 
-    prev_img.src = URL.createObjectURL(event.target.files[0]);
-  
-  }
-
-  
--- script2.js
+++ script2.js
@@ -1,3 +1,5 @@
+// Image upload
+
 function previewImage(input, blockId) {
     const file = input.files[0];
 
@@ -20,6 +22,8 @@ function previewImage(input, blockId) {
     }
 }
 
+// Color picker
+
 function changeColor(input, blockId) {
     const color = input.value;
     const block = document.getElementById(blockId);
@@ -30,5 +34,83 @@ function openColorPicker(colorInputId) {
     const colorInput = document.getElementById(colorInputId);
     colorInput.click();
 }
+
+// BG gradient
+
+document.addEventListener('DOMContentLoaded', function () {
+    const gradientContainer = document.querySelector('.gradient-container');
+    const colorInput = document.getElementById('colorInputbg');
+    const colorInput2 = document.getElementById('colorInputbg2');
+    const changeColorButton = document.getElementById('changeColorButton');
+
+    function changeGradientColors() {
+        const selectedColor = colorInput.value;
+        const selectedColor2 = colorInput2.value;
+        gradientContainer.style.background = `linear-gradient(to bottom right, ${selectedColor}, ${selectedColor2})`;
+    }
+
+    changeColorButton.addEventListener('click', changeGradientColors);
+});
+
+// Border 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const borderColorInput = document.getElementById('borderColorInput');
+    const changeBorderColorButton = document.getElementById('changeBorderColorButton');
+    const boxes = document.querySelectorAll('.border-box');
+
+    function changeBorderColors() {
+        const selectedColor = borderColorInput.value;
+        boxes.forEach(box => {
+            box.style.borderColor = selectedColor;
+        });
+    }
+
+    changeBorderColorButton.addEventListener('click', changeBorderColors);
+});
+
+// Text
+
+document.addEventListener('DOMContentLoaded', function () {
+    const textColorInput = document.getElementById('textColorInput');
+    const changeTextColorButton = document.getElementById('changeTextColorButton');
+    const coloredTexts = document.querySelectorAll('.colored-text');
+
+    function changeTextColors() {
+        const selectedColor = textColorInput.value;
+        coloredTexts.forEach(text => {
+            text.style.color = selectedColor;
+        });
+    }
+
+    changeTextColorButton.addEventListener('click', changeTextColors);
+});
+
+
+// HTML2CANVA
+
+const coloringDiv = document.getElementById('coloringDiv');
+const saveButton = document.getElementById('saveButton');
+
+saveButton.addEventListener('click', function() {
+    // Use HTML2Canvas to capture the content of the div
+    html2canvas(coloringDiv).then(function(canvas) {
+        // Get the image data from the canvas
+        const imageData = canvas.toDataURL('image/png');
+
+        // Create a temporary link element
+        const downloadLink = document.createElement('a');
+        downloadLink.href = imageData;
+        downloadLink.download = 'shipmeme.png';
+
+        // Append the link to the body and trigger the click event
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+
+        // Remove the link from the body
+        document.body.removeChild(downloadLink);
+    });
+});
+
 
 
